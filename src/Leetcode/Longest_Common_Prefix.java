fd467db4bafcd03b219c5b1e4f801eedb379a2d2
Shivam Saini <shivamsaini1072005@gmail.com>
2025-06-18 19:32:02 +0530

    adding optimal sol in leetcode 14

--- src/Leetcode/Longest_Common_Prefix.java
+++ src/Leetcode/Longest_Common_Prefix.java
@@ -1,4 +1,7 @@
 package Leetcode;
+
+import java.util.Arrays;
+
 //leetcode 14
 public class Longest_Common_Prefix {
     //Brute Force    Time=> O(M*N)    Space=>O(1)
@@ -14,5 +17,16 @@ public class Longest_Common_Prefix {
             pos++;
         }
         return strs[0];
+    }
+    //Optimized Approach    Time=> O(NlogN)   Space=> O(1)
+    public String longestCommonPrefix2(String[] strs) {
+        int n=strs.length;
+        int pos=0;
+        Arrays.sort(strs);
+        String a=strs[0];
+        String b=strs[n-1];
+        int i = 0, len = Math.min(a.length(), b.length());
+        while (i < len && a.charAt(i) == b.charAt(i)) i++;  //matching so going forward
+        return a.substring(0, i);
     }
 }
