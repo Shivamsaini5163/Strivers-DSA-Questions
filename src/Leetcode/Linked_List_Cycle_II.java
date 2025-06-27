6cfbc356ea9e120845d52f8d501252852ebc1dce
Shivam Saini <shivamsaini1072005@gmail.com>
2025-06-27 23:44:38 +0530

    some mistakes solved

---
+++ src/Leetcode/Linked_List_Cycle_II.java
@@ -0,0 +1,39 @@
+package Leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+//leetcode 142
+public class Linked_List_Cycle_II {
+    //Brute Force       O(N)      O(N)
+    public ListNode detectCycle(ListNode head) {
+        ListNode temp = head;
+        Set<ListNode> nodeMap = new HashSet<>();
+        while (temp != null) {
+            if (nodeMap.contains(temp)) {
+                return temp;
+            }
+            nodeMap.add(temp);
+            temp = temp.next;
+        }
+        return null;
+    }
+    //Optimize Approach     O(N)       O(1)
+    public ListNode detectCycle2(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast)
+                break;
+        }
+        if(fast == null || fast.next == null)
+            return null;
+        slow = head;
+        while(slow!=fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
