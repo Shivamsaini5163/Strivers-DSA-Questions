7cd1c26cbc525bf616d823273890e31b8c1eb79b
Shivam Saini <shivamsaini1072005@gmail.com>
2025-07-19 00:44:01 +0530

    solved longest consecutive sequence

--- src/Leetcode/Longest_Consecutive_Sequence.java
+++ src/Leetcode/Longest_Consecutive_Sequence.java
@@ -7,44 +7,39 @@ public class Longest_Consecutive_Sequence {
     //using sorting technique
     //Time and Space Complexity    =>  O(NlogN)   O(1)
     public int longestConsecutive(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
-        int n=nums.length;
         Arrays.sort(nums);
-        int longest=1,count=0,lastSmaller=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            //when duplicates coming we do nothing just ignore it
-            if(nums[i]-1==lastSmaller){
+        int n = nums.length;
+        if (n == 0) return 0;
+        int ans = 1;
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                // skip duplicates
+                continue;
+            } else if (nums[i]-1 == nums[i - 1]) {
                 count++;
+            } else {
+                count = 1;
             }
-            else if(nums[i]!=lastSmaller){  //it means it is not fulfilling above condition and also it is not duplicates so new start comes
-                count=1;
-            }
-            lastSmaller=nums[i];
-            longest=Math.max(longest,count);
+            ans = Math.max(ans, count);
         }
-        return longest;
+        return ans;
     }
     //Better Approach using HashSet
     //Time and Space Complexity    =>  O(N)   O(N)
     public int longestConsecutive2(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
+        int n=nums.length;
         HashSet<Integer> set=new HashSet<>();
-        for(int num:nums){
-            set.add(num);
+        for(int i=0;i<n;i++){
+            set.add(nums[i]);
         }
-        int longest=Integer.MIN_VALUE;
-        for(int i:set){
-            if(!set.contains(i-1)){
+        int longest=0;
+        for(int num:set){
+            if(!set.contains(num-1)){
                 //it means it can be start of your longest consecutive sequence
                 int length=1;
                 //we check how long it can go
-                while(set.contains(i+length)){
-                    length++;
-                }
+                while(set.contains(num+length)) length++;
                 longest=Math.max(longest,length);
             }
         }
--- src/Leetcode/Set_Matrix_Zeroes.java
+++ src/Leetcode/Set_Matrix_Zeroes.java
@@ -1,30 +1,62 @@
 package Leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
 //leetcode 73
 //Time and Space Complexity =>    O(M*N) O(M+N)
 public class Set_Matrix_Zeroes {
     public void setZeroes(int[][] matrix) {
-        boolean[] zeroRows=new boolean[matrix.length];
-        boolean[] zeroCols=new boolean[matrix[0].length];
-        int i,j;
-        for(i=0;i<matrix.length;i++){
-            for(j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    zeroRows[i]=true;
-                    zeroCols[j]=true;
+        boolean[] zeroRows = new boolean[matrix.length];
+        boolean[] zeroCols = new boolean[matrix[0].length];
+        int i, j;
+        for (i = 0; i < matrix.length; i++) {
+            for (j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
                 }
             }
         }
-        for(i=0;i<matrix.length;i++){
-            for(j=0;j<matrix[0].length;j++){
-                if(zeroRows[i]){
-                    matrix[i][j]=0;
+        for (i = 0; i < matrix.length; i++) {
+            for (j = 0; j < matrix[0].length; j++) {
+                if (zeroRows[i]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        for(j=0;j<matrix[0].length;j++){
-            for(i=0;i<matrix.length;i++){
-                if(zeroCols[j]){
-                    matrix[i][j]=0;
+        for (j = 0; j < matrix[0].length; j++) {
+            for (i = 0; i < matrix.length; i++) {
+                if (zeroCols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    //    Or you can use hashset instead
+    public void setZeroes2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        Set<Integer> zeroRows = new HashSet<>();
+        Set<Integer> zeroCols = new HashSet<>();
+
+        // First pass: Identify all rows and columns that should be zeroed
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows.add(i);
+                    zeroCols.add(j);
+                }
+            }
+        }
+
+        // Second pass: Set cells to zero if in marked row or column
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (zeroRows.contains(i) || zeroCols.contains(j)) {
+                    matrix[i][j] = 0;
                 }
             }
         }
