7230506e59e431f398c4bda607fb0b1a41aa2cda
Shivam Saini <shivamsaini1072005@gmail.com>
2025-07-27 23:02:13 +0530

    implementation of stack using queue

--- src/Leetcode/Implement_Queue_using_Stacks.java
+++ src/Leetcode/Implement_Queue_using_Stacks.java
@@ -2,14 +2,15 @@ package Leetcode;
 import java.util.Stack;
 //leetcode 232
 //time and space complexity=> Amortized O(1)
-public class  Implement_Queue_using_Stacks{
+public class  Implement_Queue_using_Stacks {
+    //Method 1
     class MyQueue {
         Stack<Integer> stack1;
         Stack<Integer> stack2;
 
         public MyQueue() {
-            stack1=new Stack();
-            stack2=new Stack();
+            stack1 = new Stack();
+            stack2 = new Stack();
         }
 
         public void push(int x) {
@@ -22,8 +23,8 @@ public class  Implement_Queue_using_Stacks{
         }
 
         public int peek() {
-            if(stack2.isEmpty()){
-                while(!stack1.isEmpty()){
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
                     stack2.push(stack1.pop());
                 }
             }
@@ -31,16 +32,44 @@ public class  Implement_Queue_using_Stacks{
         }
 
         public boolean empty() {
-            return stack2.isEmpty()&&stack1.isEmpty();
+            return stack2.isEmpty() && stack1.isEmpty();
         }
     }
+    // Method 2 using Recursion
+    class MyQueue2 {
+        Stack<Integer> stack;
+        public MyQueue2() {
+            stack=new Stack<>();
+        }
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
+        public void push(int x) {
+            if(stack.isEmpty()){
+                stack.push(x);
+                return;
+            }
+            int temp=stack.pop();
+            push(x);
+            stack.push(temp);
+        }
+
+        public int pop() {
+            if (stack.isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return stack.pop();
+        }
+
+        public int peek() {
+            if (stack.isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return stack.peek();
+        }
+
+        public boolean empty() {
+            return stack.isEmpty();
+        }
+    }
 }
--- src/Leetcode/Valid_Parentheses.java
+++ src/Leetcode/Valid_Parentheses.java
@@ -9,34 +9,17 @@ public class Valid_Parentheses {
         System.out.println(isValid(s));
     }
     public static boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        char[] ch=s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-            if (ch[i]=='{'||ch[i]=='['||ch[i]=='('){
-                st.push(ch[i]);
-            } else if (ch[i]=='}') {
-                boolean val=check(st,'{');
-                if (!val){
-                    return false;
-                }
-            } else if (ch[i]==')') {
-                boolean val=check(st,'(');
-                if (!val){
-                    return false;
-                }
-            } else if (ch[i]==']') {
-                boolean val=check(st,'[');
-                if (!val){
-                    return false;
-                }
+        Stack<Character> st = new Stack<>();
+        for (char it : s.toCharArray()) {
+            if (it == '(' || it == '[' || it == '{')
+                st.push(it);
+            else {
+                if(st.isEmpty()) return false;
+                char ch = st.pop();
+                if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
+                else return false;
             }
         }
         return st.isEmpty();
-    }
-    private static boolean check(Stack<Character> st,char ch){
-        if (st.isEmpty()){
-            return false;
-        }
-        return st.pop() == ch;
     }
 }
